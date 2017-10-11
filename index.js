c9d5226d1048756c745bb1b50353a2e8f90bea56
Karthik Thirumalasetti <karthik@appsfly.io>
2017-10-10 23:22:35 -0300

    changed length of salt

--- index.js
+++ index.js
@@ -8,20 +8,16 @@ module.exports.genChecksum = function(data, key, cb) {
     crypt.gen_salt(4, function (err, salt) {
         var sha256 = crypto.createHash('sha256').update(salt+data).digest('hex');
         var check_sum = sha256+salt;
-        console.log(sha256);
-        console.log(salt);
-        var encrypted = crypt.encrypt(check_sum, key);
-        var CHECKSUMHASH = encodeURIComponent(encrypted);
-        CHECKSUMHASH = encrypted;
-        cb(undefined, CHECKSUMHASH);
+        cb(undefined, crypt.encrypt(check_sum, key));
     });
 }
 
 module.exports.verifyChecksum = function(data, key,checksumhash) {
     var checksum = crypt.decrypt(checksumhash, key);
-    var salt = checksum.substr(checksum.length - 4);
-    var sha256 = checksum.substr(0, checksum.length - 4);
-    var hash = crypto.createHash('sha256').update(salt+data).digest('hex');
+    var salt = checksum.slice(checksum.length-8, checksum.length);
+    var sha256 = checksum.slice(0, checksum.length - 8);
+    console.log(salt+data);
+    var hash = crypto.createHash('sha256').update(salt+data).digest("hex");
     if (hash === sha256) {
         return true;
     } else {
--- sample.js
+++ sample.js
@@ -1,9 +1,9 @@
 var util = require("./index");
 
-// Generate Checksum
-util.genChecksum(JSON.stringify({"hi":"hello"}), "1234567890123456", function (err, checksum) {
-    // Verify Checksum
-    util.verifyChecksum(JSON.stringify({"hi":"hello"}), "1234567890123456", checksum);
+// // Generate Checksum
+util.genChecksum("test", "1234567890123456", function (err, checksum) {
+    console.log(util.verifyChecksum("test", "1234567890123456", "KUl9Uchy4CofX023RHzMHP20UYntr0OqLtNjdm6drrutPUkxgtk1PlegVhhaPrX1GUqG7dEZsOTq1GhORMKrC5sXpYv7XJDOrWCK4e7p8SI="));
 });
+
 
 
